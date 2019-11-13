c9daab9c4a47374334392cc3acbb2c3c09cd5147
Brandon Weaver <a7xncob@gmail.com>
2019-11-13 23:38:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,3 +3,9 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 puts doc.css(".headline-26OIBN").text.strip
+
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses.each { |course|
+  puts course.text.strip
+}

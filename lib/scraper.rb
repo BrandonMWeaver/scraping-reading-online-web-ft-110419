31ce80b80f8247896ac16169f4a5a7a6f4e8fcaf
Brandon Weaver <a7xncob@gmail.com>
2019-11-13 23:35:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,4 +2,4 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css(".headline-260IBN").text.strip
+puts doc.css(".secondaryFooterItem-3bnn9v").text.strip
